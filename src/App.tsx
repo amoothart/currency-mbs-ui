370aaf494635b410844a0afd7b296e007ed75144
Nils Pehoviak <nils@pehoviak.de>
2023-05-01 19:03:21 +0200

    trade api connected

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example in TypeScript
+          Test UI
         </Typography>
         <TradeGrid />
         <Copyright />
--- src/components/TradeGrid.tsx
+++ src/components/TradeGrid.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-
-// const [tradeData, setTradeData] = useState([])
+import { getTradeNumber } from '../services/tradeService'
 
 const rows: GridRowsProp = [
   { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+];
+
+const rows1: GridRowsProp = [
+  { id: 1, col1: 'Hello', col2: 'Waarld' },
   { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
   { id: 3, col1: 'MUI', col2: 'is Amazing' },
 ];
@@ -15,9 +20,23 @@ const columns: GridColDef[] = [
 ];
 
 export default function TradeGrid() {
+
+  const [tradeData, setTradeData] = useState(rows);
+
+  function handleUpdate() {
+    alert("hi");
+    getTradeNumber()
+      .then((newnumber) => alert('we did it'))
+      .catch((err) => alert('Error'));
+    setTradeData(rows1);
+  }
+
   return (
     <div style={{ height: 300, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} />
+      <DataGrid rows={tradeData} columns={columns} />
+      <button onClick={() => {
+        handleUpdate();
+      }}>Update</button>
     </div>
   );
   }
